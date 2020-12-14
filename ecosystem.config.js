e5027e882bcc969d2fbdc6fe72c953299a96fac6
hassansaeed2020 <hassan.saeed@fudex.com.sa>
2020-12-14 16:28:31 +0200

    ecosystem-file --changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  apps: [
-    {
-      name: "nuxt",
-      script: "./node_modules/.bin/nuxt",
-      env: {
-        HOST: "0.0.0.0",
-        PORT: 12345,
-        NODE_ENV: "production"
-      }
-    }
-  ]
+    apps: [{
+        name: 'adSolider', // App name that shows in `pm2 ls`
+        exec_mode: 'cluster', // enables clustering
+        instances: 1, // or an integer
+        cwd: './', // only if using a subdirectory
+        // These are our updated properties
+        script: "./node_modules/.bin/nuxt",
+        args: "start",
+        env: {
+            HOST: "0.0.0.0",
+            PORT: 55555,
+            NODE_ENV: "production"
+        }
+
+    }],
 };
